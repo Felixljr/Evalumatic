ccaef647746323d7139044c4681a42376e033eaf
Felix Leclerc Jr <felixLJR@icloud.com>
2022-12-16 13:28:20 -0500

    clean base with functioning webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'bundle.js',
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s?css/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
