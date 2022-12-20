a2495396af4c543468f6c53a99e5c993e0a140d8
Felix Leclerc Jr <felixLJR@icloud.com>
2022-12-20 12:20:22 -0500

    dummy login done, before export feature

--- client/components/LoginComp.js
+++ client/components/LoginComp.js
@@ -1,23 +1,67 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
+import { useState } from 'react';
 
 function LoginComp() {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [isShowAlert, setShowAlert] = useState(false);
+  const [alertColor, setAlertColor] = useState('');
+  const [alertMsg, setAlertMsg] = useState('');
+
+  function handleLogin(e) {
+    e.preventDefault();
+    console.log(user);
+    if (user === 'felix' && password === '1234') {
+      setAlertColor('alert-success');
+      setAlertMsg('Login Successful');
+      setShowAlert(true);
+      //send to this location
+      setInterval(() => (window.location.href = '/dashboard'), 3000);
+    } else {
+      setAlertColor('alert-danger');
+      setAlertMsg('Username and/or Password Invalid!');
+      setShowAlert(true);
+    }
+  }
+
   return (
-      <Form>
-        <Form.Group className='mb-3' controlId='formBasicEmail'>
-          <Form.Label>Username</Form.Label>
-          <Form.Control type='email' placeholder='Username' />
-          <Form.Text className='text-muted'></Form.Text>
-        </Form.Group>
-        <Form.Group className='mb-3' controlId='formBasicPassword'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control type='password' placeholder='Password' />
-        </Form.Group>
-        <Button variant='primary' type='submit'>
-          Submit
-        </Button>
-      </Form>
+    <Form onSubmit={handleLogin}>
+      <Form.Group className='mb-3' controlId='formBasicEmail'>
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          type='text'
+          placeholder='Username'
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <Form.Text className='text-muted'></Form.Text>
+      </Form.Group>
+      <Form.Group className='mb-3' controlId='formBasicPassword'>
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type='password'
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant='primary' type='submit'>
+        Submit
+      </Button>
+      {/* Alert for saved progress and delete */}
+      <div className='Alert-Div'>
+        <div
+          className={`alert ${alertColor} ${
+            isShowAlert ? 'alert-shown' : 'alert-hidden'
+          }`}
+          onTransitionEnd={() => setShowAlert(false)}
+        >
+          {alertMsg}
+        </div>
+      </div>
+      {/* End Alert */}
+    </Form>
   );
 }
 
