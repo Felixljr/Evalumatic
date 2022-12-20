adc9b2e08f7ede161cd39adbb141e021e3806f9b
Felix Leclerc Jr <felixLJR@icloud.com>
2022-12-20 08:23:17 -0500

    alert code added to completed

--- client/pages/Completed.js
+++ client/pages/Completed.js
@@ -6,100 +6,118 @@ import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 const Completed = () => {
-  const [name, setName] = useState('');
-  const [text, setText] = useState();
+const [name, setName] = useState('');
+const [text, setText] = useState();
+const [isShowAlert, setShowAlert] = useState(false);
+const [alertColor, setAlertColor] = useState('');
+const [alertMsg, setAlertMsg] = useState('');
 
-  const handleNameSelect = async (e) => {
-    e.preventDefault();
-    await fetch(`http://localhost:3000/dashboard/completed/${name}`, {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json',
-      },
+const handleNameSelect = async (e) => {
+  e.preventDefault();
+  await fetch(`http://localhost:3000/dashboard/completed/${name}`, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+    .then((data) => {
+      return data.text();
     })
-      .then((data) => {
-        return data.text();
-      })
-      .then((data) => {
-        setText(data);
-      });
-  };
+    .then((data) => {
+      setText(data);
+    });
+};
 
-  async function updateEval(e) {
-    e.preventDefault();
-    await fetch('http://localhost:3000/dashboard/completed', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({ name, text }),
+async function updateEval(e) {
+  e.preventDefault();
+  setAlertColor('alert-success');
+  setAlertMsg('All Changes Saved!');
+  setShowAlert(true);
+  await fetch('http://localhost:3000/dashboard/completed', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({ name, text }),
+  })
+    .then((res) => {
+      return res.json();
     })
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        console.log(res);
-      });
-  }
+    .then((res) => {
+      console.log(res);
+    });
+}
 
-  async function deleteRecord(e) {
-    e.preventDefault();
-    console.log('delete triggered')
-    await fetch(`http://localhost:3000/dashboard/completed/${name}`, {
-      method: 'DELETE',
+async function deleteRecord(e) {
+  e.preventDefault();
+  setAlertColor('alert-danger');
+  setAlertMsg(`Student Record Has Been Deleted!`);
+  setShowAlert(true);
+  console.log('delete triggered');
+  await fetch(`http://localhost:3000/dashboard/completed/${name}`, {
+    method: 'DELETE',
+  })
+    .then((res) => {
+      return res.json();
     })
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        setText(`ALL RECORDS FOR ${name.toLocaleUpperCase()} DELETED!`);
-        console.log(res);
-      });
-  }
+    .then((res) => {
+      setText(`ALL RECORDS FOR ${name.toLocaleUpperCase()} DELETED!`);
+      console.log(res);
+    });
+}
 
-  return (
-    <>
-      <NavbarComp />
-      <div className='intake'>
-        <h1>Find a completed evaluation</h1>
-        <br />
-        <Form onSubmit={handleNameSelect}>
-          <Form.Group className='mb-3' controlId='formBasicEmail'>
+return (
+  <>
+    <NavbarComp />
+    <div className='search'>
+      <h1>Find a completed evaluation</h1>
+      <br />
+      <Form onSubmit={handleNameSelect}>
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <Form.Control
+            type='text'
+            placeholder='First Name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button variant='primary' type='submit'>
+          Search
+        </Button>
+        {/* Alert for saved progress and delete */}
+        <div
+          className={`alert ${alertColor} ${
+            isShowAlert ? 'alert-shown' : 'alert-hidden'
+          }`}
+          onTransitionEnd={() => setShowAlert(false)}
+        >
+          {alertMsg}
+        </div>
+        {/* End Alert */}
+        <div className='report'>
+          <FloatingLabel controlId='floatingTextarea2'>
             <Form.Control
-              type='text'
-              style={{ height: '50px', fontSize: '20px' }}
-              placeholder='First Name'
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              as='textarea'
+              placeholder='Leave a comment here'
+              style={{ height: '400px' }}
+              value={text}
+              onChange={(e) => setText(e.target.value)}
             />
-          </Form.Group>
-
-          <Button variant='primary' type='submit'>
-            Search
+          </FloatingLabel>
+        </div>
+        <div className='actionButtons'>
+          <Button variant='primary' type='submit' onClick={updateEval}>
+            Save
           </Button>
-          <div className='report'>
-            <FloatingLabel controlId='floatingTextarea2'>
-              <Form.Control
-                as='textarea'
-                placeholder='Leave a comment here'
-                style={{ height: '400px', fontSize: '20px' }}
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-              />
-            </FloatingLabel>
-          </div>
-          <div className='actionButtons'>
-            <Button variant='success' type='submit' onClick={updateEval}>
-              Save
-            </Button>
-            <Button variant='danger' type='submit' onClick={deleteRecord}>
-              Delete
-            </Button>
-          </div>
-        </Form>
-      </div>
-    </>
-  );
+          <Button variant='danger' type='submit' onClick={deleteRecord}>
+            Delete
+          </Button>
+        </div>
+      </Form>
+    </div>
+  </>
+);
 };
 
 export default Completed;
